6d2e8cdf18069511ce89cca7d5331639e7ea1608
Vitor Lany <101216359+vitorlany@users.noreply.github.com>
2023-04-05 16:45:01 -0300

    Add files via upload

---
+++ Vitor Lany/desafio03/desafio03/ContentView.swift
@@ -0,0 +1,50 @@
+//
+//  ContentView.swift
+//  desafio03
+//
+//  Created by student on 05/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var inputText: String
+    @State private var showAlert = false
+    
+    var body: some View {
+        VStack() {
+            
+            Text("Bem vindo, \(inputText)")
+                .font(.title)
+            
+            TextField("Insira", text: $inputText)
+                .padding()
+            
+            Image("hackatruck")
+                .resizable()
+                .frame(width: 250.0, height: 250.0)
+                .aspectRatio(contentMode: .fit)
+            
+            Button("Texto") {
+                showAlert = true
+            }.alert(isPresented: $showAlert) {
+                Alert(
+                    title: Text("Titulo"),
+                    message: Text("Teste")
+                )
+            }
+
+            
+        }.background(
+            Image("hackatruck")
+                .blur(radius: 5)
+                .opacity(0.2)
+        )
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Vitor Lany/desafio03/desafio03/desafio03App.swift
@@ -0,0 +1,17 @@
+//
+//  desafio03App.swift
+//  desafio03
+//
+//  Created by student on 05/04/23.
+//
+
+import SwiftUI
+
+@main
+struct desafio03App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
